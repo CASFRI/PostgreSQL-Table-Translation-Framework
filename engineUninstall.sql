e430a67383c64c05add3836defe2f1f690369555
Pierre <pierre.racine@sbf.ulaval.ca>
2019-10-16 15:29:00 -0400

    Work on #162.
    - Added a parameter to TT_Translation() to control how invalid values are reported in the log table (ALL_GROUPED, ALL_OWN_ROW or an integer).
    - Reduced the width of some lines of code.
    -

--- engineUninstall.sql
+++ engineUninstall.sql
@@ -11,7 +11,7 @@
 --                         Pierre Vernier <pierre.vernier@gmail.com>
 -------------------------------------------------------------------------------
 SELECT TT_DropAllTranslateFct();
-DROP FUNCTION IF EXISTS _TT_Translate(name, name, name, name, name, boolean, boolean, int, boolean, boolean, boolean);
+DROP FUNCTION IF EXISTS _TT_Translate(name, name, name, text, name, name, boolean, boolean, int, boolean, boolean, boolean);
 DROP FUNCTION IF EXISTS TT_Prepare(name, name, text, name, name);
 DROP FUNCTION IF EXISTS TT_Prepare(name, name, text, name);
 DROP FUNCTION IF EXISTS TT_Prepare(name, name, text);
@@ -41,10 +41,10 @@ DROP FUNCTION IF EXISTS TT_EscapeDoubleQuotesAndBackslash(text);
 DROP FUNCTION IF EXISTS TT_EscapeSingleQuotes(text);
 DROP FUNCTION IF EXISTS TT_UnsingleQuote(text);
 DROP FUNCTION IF EXISTS TT_TableExists(text, text);
-DROP FUNCTION IF EXISTS TT_LogInit(text, text, boolean);
+DROP FUNCTION IF EXISTS TT_LogInit(text, text, boolean, text);
 DROP FUNCTION IF EXISTS TT_ShowLastLog(text, text, int);
 DROP FUNCTION IF EXISTS TT_DeleteAllLogs(text, text);
-DROP FUNCTION IF EXISTS TT_Log(text, text, text, text, text, text, int, int);
+DROP FUNCTION IF EXISTS TT_Log(text, text, text, text, text, text, text, int, int);
 DROP FUNCTION IF EXISTS TT_TextFctQuery(text, text[], jsonb, boolean, boolean);
 DROP FUNCTION IF EXISTS TT_ReportError(text, name, name, name, text, text[], jsonb, text, text, int, text, boolean, boolean);
 DROP TYPE IF EXISTS TT_RuleDef;
